7a02c030481b4cb99d15720c9f397c0c6597bb37
muehlbau <thomas.muehlbauer@magnapowertrain.com>
2024-06-03 14:52:51 +0200

    removed for loop when evaluating input as there is a well-structured dictionary for fast access; simplified output format (txt instead of csv)

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import turtle
 import pandas
 from map_text import MapText
 
+OUTPUT_FILE = 'states_to_learn.txt'
+
 screen = turtle.Screen()
 screen.title("U.S. States Game")
 image = 'blank_states_img.gif'
@@ -9,25 +11,31 @@ screen.addshape(image)
 turtle.shape(image)
 
 data = pandas.read_csv('50_states.csv')
-new_dict = {details.state: (details.x, details.y) for index, details in data.iterrows()}
+new_dict = {details.state: {'x': details.x, 'y': details.y} for index, details in data.iterrows()}
 all_states = [key for key, _ in new_dict.items()]
+
+# Create an empty output file
+open(OUTPUT_FILE, 'w').close()
 
 guessed = 0
 keep_going = True
 while keep_going:
-    answer_state = screen.textinput(title=f"Guessed: {guessed}/{len(data)}", prompt="What's another state's name? ")
+    answer_state = screen.textinput(title=f"Guessed: {guessed}/{len(data)}", prompt="What's another state's name?")
     if answer_state is None:  # Cancel - Button pressed
         keep_going = False
-        pandas.DataFrame(all_states).to_csv('states_to_learn.csv')
+        # write states that were not guessed to output file
+        output_text = f"You didn't guess these {len(all_states)} states:\n{'\n'.join(all_states)}\n"
+        print(output_text)
+        with open(OUTPUT_FILE, 'a') as f:
+            f.write(output_text)
     else:
-        for state, coords in new_dict.items():
-            if answer_state.lower() == state.lower() and answer_state.title() in all_states:
-                label = MapText(answer_state.title(), coords[0], coords[1])
-                all_states.remove(answer_state.title())
-                guessed += 1
-                if guessed >= len(data):
-                    turtle.write("Congratulations!", align="center", font=("Courier", 24, "bold"))
-                    pandas.DataFrame([]).to_csv('states_to_learn.csv')
-                    keep_going = False
+        state = answer_state.title()
+        if state in all_states:
+            label = MapText(state, new_dict[state]['x'], new_dict[state]['y'])
+            all_states.remove(state)
+            guessed += 1
+            if guessed >= len(data):
+                turtle.write("Congratulations!", align="center", font=("Courier", 24, "bold"))
+                keep_going = False
 
 screen.exitonclick()
